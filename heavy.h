f2e639f7721bbd6a38c3071e0532313ed08dc97b
Israel Rios <riosisrael.g@gmail.com>
2023-06-02 23:13:45 -0600

    clase heavy incompleta, se agrego main.cpp

--- heavy.h
+++ heavy.h
@@ -11,11 +11,17 @@
 
 class HeavyContainer : public Container {
 public:
-  HeavyContainer(int, int);
+  HeavyContainer(int id, int weight);
   HeavyContainer(int, int, ContainerType);
   HeavyContainer(const HeavyContainer&);
 
   double getConsumption() const;
 };
 
+HeavyContainer::HeavyContainer(int id, int weight) :Container(id, weight >= 0 ? weight : 0, LIGHT) {}
+
+HeavyContainer::HeavyContainer(int id, int weight)
+double HeavyContainer::getConsumption() const {
+    return 3 * getWeight(); // El consumo de combustible por llevar este tipo de contenedor es 3 veces el peso del mismo
+}
 #endif
