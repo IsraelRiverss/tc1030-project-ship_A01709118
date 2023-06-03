fbb2fb8c3a9488a1f06d244554fa6058ba17cb37
Israel Rios <riosisrael.g@gmail.com>
2023-06-03 00:19:33 -0600

    Correcciones en heavy y liquid, ambas clases ya estan commpletas y testeadas. Completas y funcionan correctamente

--- heavy.h
+++ heavy.h
@@ -12,21 +12,26 @@
 class HeavyContainer : public Container {
 public:
     HeavyContainer(int id, int weight);
+    HeavyContainer(int id, int weight, ContainerType type);
     HeavyContainer(const HeavyContainer& other);
 
     double getConsumption() const;
 };
 
 HeavyContainer::HeavyContainer(int id, int weight) : Container(id, weight, HEAVY) {
-    // Invoca al constructor de la clase superior (Container) con el tipo de contenedor correcto (HEAVY).
+    // Invokes the constructor of the base class (Container) with the correct container type (HEAVY).
+}
+
+HeavyContainer::HeavyContainer(int id, int weight, ContainerType type) : Container(id, weight, type) {
+    // Invokes the constructor of the base class (Container) with the specified container type.
 }
 
 HeavyContainer::HeavyContainer(const HeavyContainer& other) : Container(other) {
-    // Invoca al constructor de la clase superior (Container) para realizar una copia del contenedor existente.
+    // Invokes the copy constructor of the base class (Container) to copy the existing container.
 }
 
 double HeavyContainer::getConsumption() const {
-    // El consumo de combustible por llevar este tipo de contenedor es 3 veces el peso del mismo.
+    // The fuel consumption for carrying this type of container is 3 times its weight.
     return 3.0 * getWeight();
 }
 #endif
--- liquid.h
+++ liquid.h
@@ -11,10 +11,23 @@
 
 class LiquidContainer : public HeavyContainer {
 public:
-  LiquidContainer(int, int);
-  LiquidContainer(const LiquidContainer&);
+    LiquidContainer(int id, int weight);
+    LiquidContainer(const LiquidContainer& other);
 
-  double getConsumption() const;
+    double getConsumption() const;
 };
+
+LiquidContainer::LiquidContainer(int id, int weight) : HeavyContainer(id, weight, LIQUID) {
+    // Invokes the constructor of the base class (HeavyContainer) with the correct container type (LIQUID).
+}
+
+LiquidContainer::LiquidContainer(const LiquidContainer& other) : HeavyContainer(other) {
+    // Invokes the constructor of the base class (HeavyContainer) to copy the existing container.
+}
+
+double LiquidContainer::getConsumption() const {
+    // The fuel consumption for carrying this type of container is 4 times its weight.
+    return 4.0 * getWeight();
+}
 
 #endif
