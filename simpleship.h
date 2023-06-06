352700915d8fc0c74ea94772d5e24277f4292ece
Israel Rios <riosisrael.g@gmail.com>
2023-06-06 16:51:18 -0600

    Clase simpleShip completa y terminada, no testeada

--- simpleship.h
+++ simpleship.h
@@ -27,4 +27,31 @@ public:
   virtual std::string toString() const = 0;
 };
 
+SimpleShip::SimpleShip(int id) {
+    this -> id = id;
+}
+
+SimpleShip::SimpleShip(const SimpleShip & other) {
+    this -> id = other.id;
+}
+
+int SimpleShip::getId() const {
+    return id;
+}
+
+bool SimpleShip::operator==(const SimpleShip*other){
+    return (id==other -> id);
+}
+
+bool SimpleShip::operator==(const SimpleShip&other) {
+    return (id==other.id);
+}
+
+bool SimpleShip::operator<(const SimpleShip*other) {
+    return (id<other -> id);
+}
+
+bool SimpleShip::operator<(const SimpleShip&other) {
+    return (id<other.id);
+}
 #endif
